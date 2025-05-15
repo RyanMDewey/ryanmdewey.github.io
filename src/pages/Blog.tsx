3e77225da439c8823eb147f8a277acc8fa08fa11
ByteOfHeaven <Ryanmichaeldewey@gmail.com>
2025-05-15 16:40:40 -0500

    Update Blog.tsx

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Blog: React.FC = () => (
+  <main className="blog-page">
+    <h1 className="glitch">Blog</h1>
+    <p>Coming soon: written explorations of intelligence, evolution, and design.</p>
+  </main>
+);
+
+export default Blog;
