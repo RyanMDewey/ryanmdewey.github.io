006a29347e452caedc74c28d9e353f977eb41e97
ByteOfHeaven <Ryanmichaeldewey@gmail.com>
2025-05-15 16:42:24 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import Blog from "../pages/Blog";
-import Academy from "../pages/Academy";
-import Resume from "../pages/Resume";
-import Contact from "../pages/Contact";
-import Skills from "../pages/Skills";
-import Projects from "../pages/Projects";
-import Experience from "../pages/Experience";
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Contact from '../pages/Contact';
+import Resume from '../pages/Resume';
+import Skills from '../pages/Skills';
+import Projects from '../pages/Projects';
+import Experience from '../pages/Experience';
+import Blog from '../pages/Blog';
+import Academy from '../pages/Academy';
 
-export default function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/academy" element={<Academy />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/skills" element={<Skills />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/experience" element={<Experience />} />
-    </Routes>
-  );
-}
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/resume" element={<Resume />} />
+    <Route path="/skills" element={<Skills />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/experience" element={<Experience />} />
+    <Route path="/blog" element={<Blog />} />
+    <Route path="/academy" element={<Academy />} />
+  </Routes>
+);
+
+export default AppRoutes;
