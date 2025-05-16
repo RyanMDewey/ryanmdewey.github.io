3328fb663410a3cdecb0c65ef8ac1b062a82ce80
ByteOfHeaven <Ryanmichaeldewey@gmail.com>
2025-05-16 01:25:26 -0500

    Update Academy.tsx

--- src/pages/Academy.tsx
+++ src/pages/Academy.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Academy: React.FC = () => (
-  <main className="academy-page">
+  <section className="page-content">
     <h1 className="glitch">Academy</h1>
     <p>Courses, tools, and frameworks to help you architect intelligent systems.</p>
-  </main>
+  </section>
 );
 
 export default Academy;
