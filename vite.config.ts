9e7cfd6a8647adc11d108bbf0813b67033a5e6b0
ByteOfHeaven <Ryanmichaeldewey@gmail.com>
2025-05-16 02:06:05 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.tsx'), // <== FIX: Vite knows your real entry point
-    }
+    outDir: 'build'
   },
   server: {
-    port: 3000
+    port: 3000,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['www.ryanmdewey.com']
   }
 });
